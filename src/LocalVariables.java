3ba37d7cc375cb28ca6e68826d1066f8ca5bba20
McLoy <tkachenko.vlad@hotmail.com>
2016-03-16 23:40:35 +0200

    16.03.2016

---
+++ src/LocalVariables.java
@@ -0,0 +1,12 @@
+/**
+ * Created by User on 16.03.2016.
+ */
+public class LocalVariables {
+
+    public static void main(String[] args) {
+        int y = 10;
+        int x = 5;
+//        int reply = x+y;
+        System.out.println(x+y);
+    }
+}
