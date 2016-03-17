73d253a9ea69fcb8242438f06be733b8f69c243f
McLoy <tkachenko.vlad@hotmail.com>
2016-03-17 16:27:29 +0200

    update

--- src/ch1/Questions/WaterBottle.java
+++ src/ch1/Questions/WaterBottle.java
@@ -9,6 +9,14 @@ public class WaterBottle {
     private boolean empty;
 
     public static void main(String[] args) {
+
+//        short numPets = 5;
+//        int numGrains = 5.6;
+//        String name = "Scruffy";
+//        numPets.length();
+//        numGrains.length();
+//        name.length();
+
         WaterBottle wb = new WaterBottle();
         System.out.print("Empty = " + wb.empty);
         System.out.print(", Brand = " + wb.brand);
