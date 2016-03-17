4d20b3dc5f3b78c54e292165e71b8977d57c019c
McLoy <tkachenko.vlad@hotmail.com>
2016-03-17 16:50:44 +0200

    update

---
+++ src/ch1/Questions/aquarium/Tank.java
@@ -0,0 +1,7 @@
+package ch1.Questions.aquarium;
+
+/**
+ * Created by User on 17.03.2016.
+ */
+public class Tank {
+}
---
+++ src/ch1/Questions/visitor/AquariumVisitor.java
@@ -0,0 +1,12 @@
+package ch1.Questions.visitor;
+
+import ch1.Questions.aquarium.jellies.Jelly;
+
+/**
+ * Created by User on 17.03.2016.
+ */
+public class AquariumVisitor {
+    public void admire(Jelly jelly) {
+
+    }
+}
