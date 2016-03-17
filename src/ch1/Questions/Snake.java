97f91c9367ed82f048115ff8ea9d98ea46e6bb78
McLoy <tkachenko.vlad@hotmail.com>
2016-03-17 16:42:50 +0200

    update

---
+++ src/ch1/Questions/Snake.java
@@ -0,0 +1,16 @@
+package ch1.Questions;
+
+/**
+ * Created by User on 17.03.2016.
+ */
+public class Snake {
+
+    public static void shed(boolean time) {
+        String result = "done";
+        if (time){
+
+        }
+        System.out.println(result);
+
+    }
+}
