0c287472b354010667c8fadc963f03f839dfbb8d
McLoy <tkachenko.vlad@hotmail.com>
2016-03-17 17:22:03 +0200

    update

---
+++ src/ch1/Questions/BirdDisplay.java
@@ -0,0 +1,15 @@
+package ch1.Questions;
+
+/**
+ * Created by User on 17.03.2016.
+ */
+public class BirdDisplay {
+//    public static void main(String[] name) {
+//        System.out.println(name[1]);
+//
+//    }
+public static void main(String[] args) {
+
+}
+
+}
