ce13e023a7dabc53f8a3c5702d7770a2a1012845
McLoy <tkachenko.vlad@hotmail.com>
2016-03-17 17:32:15 +0200

    update

---
+++ src/ch1/Questions/Price.java
@@ -0,0 +1,11 @@
+package ch1.Questions;
+
+/**
+ * Created by User on 17.03.2016.
+ */
+public class Price {
+    public void admission(){
+        int amount = 0xE;
+        System.out.println(amount);
+    }
+}
