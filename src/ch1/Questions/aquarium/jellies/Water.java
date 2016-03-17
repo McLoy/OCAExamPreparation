667ce768da137b39c1c5188f63c0eabc33db7104
McLoy <tkachenko.vlad@hotmail.com>
2016-03-17 17:01:38 +0200

    update

---
+++ src/ch1/Questions/aquarium/jellies/Water.java
@@ -0,0 +1,8 @@
+package ch1.Questions.aquarium.jellies;
+
+/**
+ * Created by User on 17.03.2016.
+ */
+public class Water {
+    boolean salty = true;
+}
---
+++ src/ch1/Questions/employee/WaterFiller.java
@@ -0,0 +1,12 @@
+package ch1.Questions.employee;
+
+/**
+ * Created by User on 17.03.2016.
+ */
+
+//import ch1.Questions.aquarium.Water;
+//import ch1.Questions.aquarium.jellies.Water;
+
+public class WaterFiller {
+//    Water water;
+}
